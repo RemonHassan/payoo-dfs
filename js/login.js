a345ae51a758b57d0460b0a0bf69a3788d5620de
RemonHassan <fmremonhassan203@gmail.com>
2024-09-19 00:05:57 +0600

    added home page

--- js/login.js
+++ js/login.js
@@ -1,9 +1,32 @@
 // console.log('logIn added')
 
+// document.getElementById('button-login').addEventListener('click',function(event){
+//     event.preventDefault();
+
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber,pinNumber);
+
+//     if(phoneNumber === '5' && pinNumber === '1234'){
+//         console.log('You are logged in');
+//     }
+//     else{
+//         alert('Wroong Phone Number Or Pin');
+//     }
+// })
+
+
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
-    console.log('logdeeeddddddd');
-
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('Logged');
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('Wrong')
+    }
 })
