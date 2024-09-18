b8e60637428ac28ed5e9122e957fb5048ee4646d
RemonHassan <fmremonhassan203@gmail.com>
2024-09-19 01:23:45 +0600

    Finished add money

--- js/home.js
+++ js/home.js
@@ -5,4 +5,16 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
 
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+        const balance = document.getElementById('account-balance').innerText;
+        
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('wrong');
+    }
 })
